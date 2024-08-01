aa425895fd6c708cdcd91b2980cb341c1ff7e738
Ishu Gupta <guptarhsu999@gmail.com>
2024-08-01 21:57:37 +0530

    Add: edit feature for the row's value

--- js-folder/form.js
+++ js-folder/form.js
@@ -1,6 +1,33 @@
 $(document).ready(function () {
+  let selectedDay, selectedMonth, selectedYear, selectedSalary;
+  if(localStorage.getItem('data')){
+    data = JSON.parse(localStorage.getItem('data'));
+    $('#name').val(data.name);
+    $(`input[class='form-check-input'][value='${data.profilePic}']`).prop(
+      "checked",
+      true
+    );
+    $(`input[class='form-check-input'][value='${data.gender}']`).prop(
+      "checked",
+      true
+    );
+    const department = data.department;
+    for(let i = 0; i < department.length; i++){
+      $(`input[name="department"][value='${department[i]}']`).prop("checked",true);
+    }
+    selectedSalary = data.salary;
+    $(".dropdown-text").text(data.salary); 
+    const date = data.startDate.split(" ");
+    console.log(date[0] + " " + date[1] + " " + date[2]);
+    selectedDay = date[0];
+    selectedMonth = date[1];
+    selectedYear = date[2];
+    $(".dropdown-day").text(date[0]);
+    $(".dropdown-month").text(date[1]);
+    $(".dropdown-year").text(date[2]);
+    $("#note").val(data.note);
 
-  
+  }
   // Dropdown menu for day, month, year and salary
   for (let i = 1; i <= 31; i++) {
     $("#day-dropdown").append(
@@ -9,18 +36,18 @@ $(document).ready(function () {
   }
 
   const months = [
-    "January",
-    "February",
-    "March",
-    "April",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
     "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
   ];
   months.forEach((month) => {
     $("#month-dropdown").append(
@@ -28,38 +55,54 @@ $(document).ready(function () {
     );
   });
 
-  let selectedDay, selectedMonth, selectedYear, selectedSalary;
+  
 
   // Event listener for day dropdown
   $("#day-dropdown").on("click", ".dropdown-item", function () {
-    selectedDay = $(this).text();
+    if(selectedDay != undefined){
+      selectedDay = $(this).text();
+    }else{
+      selectedDay = $(this).text();
+    }
     $(this)
       .closest(".btn-group")
-      .find(".dropdown-toggle .dropdown-text")
+      .find(".dropdown-toggle .dropdown-day")
       .text(selectedDay);
   });
 
   // Event listener for month dropdown
   $("#month-dropdown").on("click", ".dropdown-item", function () {
-    selectedMonth = $(this).text();
+    if(selectedMonth != undefined){
+      selectedMonth = $(this).text();
+    }else{
+      selectedMonth = $(this).text();
+    }
     $(this)
       .closest(".btn-group")
-      .find(".dropdown-toggle .dropdown-text")
+      .find(".dropdown-toggle .dropdown-month")
       .text(selectedMonth);
   });
 
   // Event listener for year dropdown
   $("#year-dropdown").on("click", ".dropdown-item", function () {
-    selectedYear = $(this).text();
+    if(selectedYear != undefined){
+      selectedYear = $(this).text();
+    }else{
+      selectedYear = $(this).text();
+    }
     $(this)
       .closest(".btn-group")
-      .find(".dropdown-toggle .dropdown-text")
+      .find(".dropdown-toggle .dropdown-year")
       .text(selectedYear);
   });
 
   // Event listener for salary dropdown
   $("#salary").on("click", ".dropdown-item", function () {
-    selectedSalary = $(this).text();
+    if(selectedSalary != undefined){
+      selectedSalary = $(this).text();
+    }else {
+      selectedSalary = $(this).text();
+    }
     $(this)
       .closest(".dropdown")
       .find(".dropdown-toggle .dropdown-text")
@@ -68,13 +111,6 @@ $(document).ready(function () {
 
   $("form").submit(function (e) {
     e.preventDefault();
-    var name = $("#name").val();
-    if (name == "") {
-      $("#name").after(
-        '<div id="name-error" style="color: red; font-size: 10px">Please fill name</div>'
-      );
-      return;
-    }
     var department = [];
     var profileImage;
     var gender;
@@ -101,6 +137,22 @@ $(document).ready(function () {
     };
     var obj = JSON.stringify(employeeData);
     console.log(employeeData);
+
+    if(localStorage.getItem('data')){
+      data = JSON.parse(localStorage.getItem('data'));
+      $.ajax({
+        url: "http://localhost:3000/employees/" + data.id,
+        type: "PUT",
+        data: obj,
+        contentType: "application/json",
+        success: function (data) {
+          localStorage.removeItem('data');
+          window.location.href = "index.html";
+        },error(err){
+          console.log(err);
+        }
+      });
+    }else
     createEmployee(obj);
   });
 });
@@ -120,3 +172,82 @@ function createEmployee(obj) {
     }
   });
 }
+
+// function updateEmployee(id) {
+
+//   let selectedDay, selectedMonth, selectedYear, selectedSalary;
+
+//   // Event listener for day dropdown
+//   $("#day-dropdown").on("click", ".dropdown-item", function () {
+//     selectedDay = $(this).text();
+//     $(this)
+//       .closest(".btn-group")
+//       .find(".dropdown-toggle .dropdown-text")
+//       .text(selectedDay);
+//   });
+
+//   // Event listener for month dropdown
+//   $("#month-dropdown").on("click", ".dropdown-item", function () {
+//     selectedMonth = $(this).text();
+//     $(this)
+//       .closest(".btn-group")
+//       .find(".dropdown-toggle .dropdown-text")
+//       .text(selectedMonth);
+//   });
+
+//   // Event listener for year dropdown
+//   $("#year-dropdown").on("click", ".dropdown-item", function () {
+//     selectedYear = $(this).text();
+//     $(this)
+//       .closest(".btn-group")
+//       .find(".dropdown-toggle .dropdown-text")
+//       .text(selectedYear);
+//   });
+
+//   // Event listener for salary dropdown
+//   $("#salary").on("click", ".dropdown-item", function () {
+//     selectedSalary = $(this).text();
+//     $(this)
+//       .closest(".dropdown")
+//       .find(".dropdown-toggle .dropdown-text")
+//       .text(selectedSalary);
+//   });
+
+//   var department = [];
+//   var profileImage;
+//   var gender;
+//   var x;
+//   x = $('input[name="department"]');
+//   for (const dep of x) {
+//     if (dep.checked) {
+//       department.push(dep.value);
+//     }
+//   }
+//   profileImage = $("input[name='profileImage']:checked").val();
+//   gender = $("input[name='gender']:checked").val();
+//   var startDate = `${selectedDay} ${selectedMonth} ${selectedYear}`;
+
+//   var employeeData = {
+//     name: $("#name").val(),
+//     profilePic: profileImage,
+//     gender: gender,
+//     department: department,
+//     salary: selectedSalary,
+//     startDate: startDate,
+//     note: $("#note").val(),
+//   };
+//   var obj = JSON.stringify(employeeData);
+  
+
+//   $.ajax({
+//     url: "http://localhost:3000/employees/" + id,
+//     type: "PUT",
+//     success: function (data) {
+//       console.log(data);
+//       window.location.href = "index.html";
+//     },error(err){
+//       console.log(err);
+//     }
+//   });
+
+// }
