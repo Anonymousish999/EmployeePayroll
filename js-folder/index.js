c17764406404a0ed0df565257891ca5387bbec10
Ishu Gupta <guptarhsu999@gmail.com>
2024-08-01 23:02:53 +0530

    Add: Search the employee from row

--- js-folder/index.js
+++ js-folder/index.js
@@ -73,9 +73,35 @@ function editUser(){
         },error(err){
             console.log(err);
         }
-    });
-    
-    
-            
-    
+    });    
+}
+function openSearchBar() {
+  if (document.getElementById("myInput").style.display == "")
+    document.getElementById("myInput").style.display = "none";
+  else document.getElementById("myInput").style.display = "";
+}
+function onSearch() {
+  var input, filter, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("employeetable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    td2 = tr[i].getElementsByTagName("td")[2];
+    if (td || td2) {
+      txtValue = td.textContent || td.innerText;
+      txtgender = td2.textContent || td2.innerText;
+      console.log(filter);
+      if (
+        txtValue.toUpperCase().indexOf(filter) > -1 ||
+        txtgender.toUpperCase().startsWith(filter) == 1
+      ) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
 }
